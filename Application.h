88f0cf1c879ae8ef8b8385d332679375a6a84137
Alexander Tsirel <noma4i@gmail.com>
2015-03-05 22:45:07 +1100

    all files of Xcode direct build

---
+++ Application.h
@@ -0,0 +1,10 @@
+#ifndef Telegram_Application_h
+#define Telegram_Application_h
+
+
+#define API_ID 0000 // you can create new app on my.telegram.org
+#define API_HASH @"" // you can create new app on my.telegram.org
+#define HOCKEY_APP_IDENTIFIER @"" // hocheckey app
+#define HOCKEY_APP_COMPANY @""  // hocheckey app
+#define BUNDLE_IDENTIFIER @"ru.keepcoder.Telegram"  // bundle name
+#endif
